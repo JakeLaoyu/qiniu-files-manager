b4c72fa087031d970ed4fd97059d1e858e1c4829
Jake <yucj@dxy.cn>
2017-11-20 09:32:46 +0800

    取消跨域

--- server/app.js
+++ server/app.js
@@ -17,12 +17,12 @@ var dbUrl = 'mongodb://localhost/qiniumanager';
 var app = express();
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//
+//   next();
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
