24b5a10be6d72926bce98949b69d80a6de986f18
Jake <yucj@dxy.cn>
2018-03-16 13:35:49 +0800

    项目重构，使用vue-cli 重新生成项目

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
-# 七牛图形化管理 web 端
+# qiniu-images-manager
 
-# 技术栈
-
-vue、nodejs、expressjs、七牛SDK
-
-# 文件目录
-
-* src： vue文件目录
-* server： node、expressjs文件目录
-
-# todo
-
-* 批量操作
-* 接入 vuex
+重构中
--- src/main.js
+++ src/main.js
@@ -1,60 +1,15 @@
-import Vue from 'vue';
-import iView from 'iview';
-import VueRouter from 'vue-router';
-import Routers from './router';
-import Vuex from 'vuex';
-import Util from './libs/util';
-import App from './app.vue';
-import VueLazyLoadImg from 'vue-lazy-load-img'
-import 'iview/dist/styles/iview.css';
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
+Vue.config.productionTip = false
 
-Vue.use(VueRouter);
-Vue.use(Vuex);
-
-Vue.use(iView);
-Vue.use(VueLazyLoadImg)
-
-
-
-// 路由配置
-const RouterConfig = {
-  mode: 'history',
-  routes: Routers
-};
-const router = new VueRouter(RouterConfig);
-
-router.beforeEach((to, from, next) => {
-  iView.LoadingBar.start();
-  Util.title(to.meta.title);
-  next();
-});
-
-router.afterEach(() => {
-  iView.LoadingBar.finish();
-  window.scrollTo(0, 0);
-});
-
-
-const store = new Vuex.Store({
-  state: {
-
-  },
-  getters: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
-});
-
-
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router: router,
-  store: store,
-  render: h => h(App)
-});
+  router,
+  components: { App },
+  template: '<App/>'
+})
