9c63241108803d40d4bd1e56016a3f558d0d6e48
Jake <Jake.Laoyu@gmail.com>
2018-10-11 13:14:48 +0800

    feat: 新版本通知

--- src/app.vue
+++ src/app.vue
@@ -37,7 +37,11 @@ export default {
   created () {
     // 更新为 通过 ak、sk获取数据后，需要重新拉数据
     if (this.currentBucket.domain && !this.currentBucket.domains) {
-      this.$Message.info('新版更细，需要重新获取数据')
+      this.$Notice.open({
+        title: '通知',
+        desc: '新版更细，需要设置AccessKey、SecretKey',
+        duration: 3
+      })
       this.emptyState()
     }
   }
