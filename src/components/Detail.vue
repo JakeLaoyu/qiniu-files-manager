9a0bfb4d99f77e3214d562629382dbc56c5187c1
Jake <Jake.Laoyu@gmail.com>
2021-11-28 15:21:51 +0800

    fix: 复制链接 encode

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -161,7 +161,7 @@ export default {
     copyUrl () {
       if (this.clipboard) this.clipboard.destroy()
       this.clipboard = new Clipboard('#copyBtn', {
-        text: () => this.fileUrl
+        text: () => encodeURIComponent(this.fileUrl)
       })
       this.clipboard.on('success', e => {
         this.$Message.success('复制成功 ' + this.fileUrl)
