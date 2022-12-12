2a4db18a0db21925b8133a41d1c94268620bc332
Jake Yu <jake.yu@zoom.us>
2022-12-12 16:12:06 +0800

    feat(server): redis

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AppController } from './controllers/app.controller';
+import { AppService } from './service/app.service';
 
 @Module({
   imports: [],
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Redis from 'redis';
+import * as RedisStore from 'connect-redis';
+import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const { REDIS_HOST = '127.0.0.1', REDIS_PORT = 6379 } = process.env;
+
+  const port = process.env.PORT || '2017';
+  const dbUrl = `redis://${REDIS_HOST}:${REDIS_PORT}`;
+
+  const redisClient = Redis.createClient({ legacyMode: true, url: dbUrl });
+  redisClient.connect().catch(console.error);
+
+  app.use(cookieParser());
+  app.use(
+    session({
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30,
+      },
+      resave: true,
+      saveUninitialized: true,
+      secret: '0A6194FD0E695254A939A25C3D868D2C',
+      // session持久化，存在到mongodb中
+      store: new (RedisStore(session))({
+        client: redisClient,
+        prefix: 'qim:sess:',
+      }),
+    }),
+  );
+
+  await app.listen(port);
+
+  console.log(`Nest application started on http://localhost:${port}`);
 }
+
 bootstrap();
