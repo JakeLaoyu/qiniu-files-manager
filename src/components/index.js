f6eac8b5377f3caa283f0a971dd6b2c3f3bea535
Jake Yu <jake.yu@zoom.us>
2022-12-12 14:42:59 +0800

    chore: replace to vue3 & nestjs & pnpm workspace

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  Modal: () => import('./Modal.vue'),
-  Upload: () => import('./Upload.vue'),
-  ImageItem: () => import('./ImageItem.vue'),
-  Detail: () => import('./Detail.vue'),
-  Icon: () => import('./Icon.vue'),
-  SearchItem: () => import('./SearchItem.vue')
-}
