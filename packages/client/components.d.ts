857ba991abe63678674829301b663f96fc1697c5
Jake <Jake.Laoyu@gmail.com>
2023-01-01 14:48:38 +0800

    feat: add button to back top (#78)

--- packages/client/components.d.ts
+++ packages/client/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ABackTop: typeof import('@arco-design/web-vue')['BackTop']
     ABreadcrumb: typeof import('@arco-design/web-vue')['Breadcrumb']
     ABreadcrumbItem: typeof import('@arco-design/web-vue')['BreadcrumbItem']
     AButton: typeof import('@arco-design/web-vue')['Button']
