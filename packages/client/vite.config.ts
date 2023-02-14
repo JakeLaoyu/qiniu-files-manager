62b7bedc4277f17e016aeb297e6d931822370b8a
Jake <Jake.Laoyu@gmail.com>
2023-02-14 06:20:00 +0000

    feat: pwa[D

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ArcoResolver } from "unplugin-vue-components/resolvers";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,13 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "七牛文件管理",
+        short_name: "qim",
+      },
+    }),
     vue(),
     AutoImport({
       resolvers: [ArcoResolver()],
